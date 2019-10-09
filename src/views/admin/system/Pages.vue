a6615da10109a213b7bc08e70d4e6f2146b987a3
nlovesye <894765963@qq.com>
2019-10-09 18:09:56 +0800

    init

---
+++ src/views/admin/system/Pages.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="system-pages">
+    THIS IS system-pages
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Pages',
+  created () {
+    // this.$router.push('/login')
+  }
+}
+</script>
---
+++ utils/index.js
@@ -0,0 +1,32 @@
+const arrToTree = (arr) => {
+  let roots = arr.filter(item => !item.parent).map(({ name, value }) => ({ label: name, value }))
+  const childrens = arr.filter(item => item.parent).map(({ parent, name, value }) => ({ label: name, parent, value }))
+  const toTree = (pArr, cArr) => {
+    pArr.forEach(parent => {
+      for (let i = 0; i < cArr.length; i++) {
+        const cur = cArr[i]
+        if (cur.parent === parent.value) {
+          const temp = cArr.splice(i, 1)
+          i--
+          toTree([cur], cArr)
+          parent.children = parent.children ? parent.children.concat(temp) : temp
+        }
+      }
+    })
+  }
+  const format = arr => {
+    return arr.map(({ parent, ...rest }) => {
+      if (rest.children) {
+        rest.children = format(rest.children)
+      }
+      return rest
+    })
+  }
+  toTree(roots, childrens)
+  roots = format(roots)
+  return roots
+}
+
+module.exports = {
+  arrToTree
+}
