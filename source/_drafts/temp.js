bca3b03819f6ea8c7290e05eff1d8a21175981f3
🌏🏡锁笼 <894765963@qq.com>
2025-01-17 10:51:46 +0800

    feat: save

--- source/_drafts/temp.js
+++ source/_drafts/temp.js
@@ -1,3 +1,25 @@
 function a() {
   console.log(233);
 }
+
+function fn() {
+  setTimeout(() => {
+    console.log(6);
+  });
+
+  new Promise((resolve) => {
+    resolve(false);
+  })
+    .then(() => console.log(3))
+    .then(() => console.log(4))
+    .catch(() => console.log(5));
+
+  console.log(1);
+}
+
+function fn2() {
+  console.log(2);
+}
+
+fn();
+fn2();
